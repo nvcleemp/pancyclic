d0b4f86b7e93cfd40b3040447ecac5a90cd3a43b
nvcleemp <nico.vancleemput@gmail.com>
2014-07-23 17:52:55 +0200

    Some boilerplate code for the program

---
+++ pancyclic.c
@@ -0,0 +1,72 @@
+/*
+ * Main developer: Nico Van Cleemput
+ * 
+ * Copyright (C) 2014 Nico Van Cleemput.
+ * Licensed under the GNU GPL, read the file LICENSE.txt for details.
+ */
+
+/* This program reads simple graphs from standard in and
+ * checks whether they are pancyclic.
+ * 
+ * 
+ * Compile with:
+ * 
+ *     cc -o pancyclic -O4 pancyclic.c
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "shared/multicode_base.h"
+#include "shared/multicode_input.h"
+#include "shared/multicode_output.h"
+
+//====================== USAGE =======================
+
+void help(char *name) {
+    fprintf(stderr, "The program %s checks whether simple graphs are pancyclic.\n\n", name);
+    fprintf(stderr, "Usage\n=====\n");
+    fprintf(stderr, " %s [options]\n\n", name);
+    fprintf(stderr, "\nThis program can handle graphs up to %d vertices and degrees up to %d.\n\n", MAXN, MAXVAL);
+    fprintf(stderr, "Valid options\n=============\n");
+    fprintf(stderr, "    -h, --help\n");
+    fprintf(stderr, "       Print this help and return.\n");
+}
+
+void usage(char *name) {
+    fprintf(stderr, "Usage: %s [options]\n", name);
+    fprintf(stderr, "For more information type: %s -h \n\n", name);
+}
+
+int main(int argc, char *argv[]) {
+
+    /*=========== commandline parsing ===========*/
+
+    int c;
+    char *name = argv[0];
+    static struct option long_options[] = {
+         {"help", no_argument, NULL, 'h'}
+    };
+    int option_index = 0;
+
+    while ((c = getopt_long(argc, argv, "h", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 'h':
+                help(name);
+                return EXIT_SUCCESS;
+            case '?':
+                usage(name);
+                return EXIT_FAILURE;
+            default:
+                fprintf(stderr, "Illegal option %c.\n", c);
+                usage(name);
+                return EXIT_FAILURE;
+        }
+    }
+
+    
+    return EXIT_SUCCESS;
+}
